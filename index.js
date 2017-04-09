946b92e7a1dc4d7f51b3dbc3ed93c71d0220ba30
jstube2 <jstube2@gmail.com>
2017-04-09 11:29:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var handle = {};
 handle['/'] = home;
+handle['/authorize'] = authorize;
 
 server.start(router.route, handle);
 
@@ -7,5 +8,17 @@ function home(response, request) {
   console.log('Request handler \'home\' was called.');
   response.writeHead(200, {'Content-Type': 'text/html'});
   response.write('<p>Please <a href="' + authHelper.getAuthUrl() + '">sign in</a> with your Office 365 or Outlook.com account.</p>');
+  response.end();
+}
+var url = require('url');
+function authorize(response, request) {
+  console.log('Request handler \'authorize\' was called.');
+  
+  // The authorization code is passed as a query parameter
+  var url_parts = url.parse(request.url, true);
+  var code = url_parts.query.code;
+  console.log('Code: ' + code);
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('<p>Received auth code: ' + code + '</p>');
   response.end();
 }
