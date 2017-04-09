b4200f8075ceb057be4b5e2a94404f83d246bc85
jstube2 <jstube2@gmail.com>
2017-04-09 11:28:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-var server = require('./server');
-var router = require('./router');
-
 var handle = {};
 handle['/'] = home;
 
@@ -9,6 +6,6 @@ server.start(router.route, handle);
 function home(response, request) {
   console.log('Request handler \'home\' was called.');
   response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write('<p>Please <a href='#'>sign in</a> with your Office 365 or Outlook.com account.</p>');
+  response.write('<p>Please <a href="' + authHelper.getAuthUrl() + '">sign in</a> with your Office 365 or Outlook.com account.</p>');
   response.end();
 }
