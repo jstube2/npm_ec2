7bfe5864795e834a60e596d65af2bf952598db1a
jstube2 <jstube2@gmail.com>
2017-04-13 21:42:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -138,3 +138,51 @@ function getAccessToken(request, response, callback) {
     callback(null, access_token);
   }
 }
+function mail(response, request) {
+  getAccessToken(request, response, function(error, token) {
+    console.log('Token found in cookie: ', token);
+    var email = getValueFromCookie('node-tutorial-email', request.headers.cookie);
+    console.log('Email found in cookie: ', email);
+    if (token) {
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write('<div><h1>Your inbox</h1></div>');
+      
+      var queryParams = {
+        '$select': 'Subject,ReceivedDateTime,From,IsRead',
+        '$orderby': 'ReceivedDateTime desc',
+        '$top': 10
+      };
+      
+      // Set the API endpoint to use the v2.0 endpoint
+      outlook.base.setApiEndpoint('https://outlook.office.com/api/v2.0');
+      // Set the anchor mailbox to the user's SMTP address
+      outlook.base.setAnchorMailbox(email);
+      
+      outlook.mail.getMessages({token: token, odataParams: queryParams},
+        function(error, result){
+          if (error) {
+            console.log('getMessages returned an error: ' + error);
+            response.write('<p>ERROR: ' + error + '</p>');
+            response.end();
+          } else if (result) {
+            console.log('getMessages returned ' + result.value.length + ' messages.');
+            response.write('<table><tr><th>From</th><th>Subject</th><th>Received</th></tr>');
+            result.value.forEach(function(message) {
+              console.log('  Subject: ' + message.Subject);
+              var from = message.From ? message.From.EmailAddress.Name : 'NONE';
+              response.write('<tr><td>' + from + 
+                '</td><td>' + (message.IsRead ? '' : '<b>') + message.Subject + (message.IsRead ? '' : '</b>') +
+                '</td><td>' + message.ReceivedDateTime.toString() + '</td></tr>');
+            });
+            
+            response.write('</table>');
+            response.end();
+          }
+        });
+    } else {
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write('<p> No token found in cookie!</p>');
+      response.end();
+    }
+  });
+}
