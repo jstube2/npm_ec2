22732c0b9e052a25be23ffc463b08c7735e8ed28
jstube2 <jstube2@gmail.com>
2017-04-22 13:07:17 -0500

    Update authHelper.js

--- authHelper.js
+++ authHelper.js
@@ -11,7 +11,7 @@ var credentials = {
 };
 var oauth2 = require('simple-oauth2').create(credentials);
 
-var redirectUri = 'https://ec2-34-205-29-189.compute-1.amazonaws.com:4443/';
+var redirectUri = 'https://ec2-34-205-29-189.compute-1.amazonaws.com:4443/authorize';
 
 // The scopes the app requires
 // The scopes the app requires
